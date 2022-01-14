74996f9b57e1154d0108687d2a5aa730214f47f0
Jinhao Liu <jinhaoliu18@hotmail.com>
2022-01-14 05:24:11 +0000

    add cifar gtrsb train files

--- dataloader.py
+++ dataloader.py
@@ -10,6 +10,33 @@ def normalize(Xmin, Xmax, X):
         for j in range(0, len(X[0])):
             X[i][j] = (X[i][j] - Xmin[j]) / (Xmax[j] - Xmin[j])
     return X
+def gray_scale(image):
+    """
+    Convert images to gray scale.
+        Parameters:
+            image: An np.array compatible with plt.imshow.
+    """
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def local_histo_equalize(image):
+    """
+    Apply local histogram equalization to grayscale images.
+        Parameters:
+            image: A grayscale image.
+    """
+    kernel = morp.disk(30)
+    img_local = rank.equalize(image, selem=kernel)
+    return img_local
+def preprocess(data):
+    """
+    Applying the preprocessing steps to the input data.
+        Parameters:
+            data: An np.array compatible with plt.imshow.
+    """
+    gray_images = list(map(gray_scale, data))
+    equalized_images = list(map(local_histo_equalize, gray_images))
+    
+    return equalized_images
 
 def load_data(dataset_name, use_imb=True):
     print("loading data")
@@ -60,4 +87,42 @@ def load_data(dataset_name, use_imb=True):
         y_test = np.asarray(le.transform(y_test)).astype(np.int32)
 
         return X_train, y_train, X_test, y_test
+    
+    elif dataset_name == 'cifar':
+        cifar = tf.keras.datasets.cifar10
+        (X_train, y_train), (X_test, y_test) = cifar.load_data()
+        X_train, X_test = X_train / 255.0, X_test / 255.0
+        
+        le = preprocessing.LabelEncoder()
+        y_train = np.asarray(le.fit_transform(y_train)).astype(np.int32)
+        y_test = np.asarray(le.transform(y_test)).astype(np.int32)
+        
+        return X_train, y_train, X_test, y_test
+
+        
+    elif dataset_name == 'gtsrb':
+        training_file = "./traffic-signs-data/train.p"
+        validation_file= "./traffic-signs-data/valid.p"
+        testing_file = "./traffic-signs-data/test.p"
+
+        with open(training_file, mode='rb') as f:
+            train = pickle.load(f)
+        #with open(validation_file, mode='rb') as f:
+            #valid = pickle.load(f)
+        with open(testing_file, mode='rb') as f:
+            test = pickle.load(f)
+            
+        # Mapping ClassID to traffic sign names
+        signs = []
+        with open('signnames.csv', 'r') as csvfile:
+            signnames = csv.reader(csvfile, delimiter=',')
+            next(signnames,None)
+            for row in signnames:
+                signs.append(row[1])
+            csvfile.close()
+        X_train, y_train = train['features'], train['labels']
+        X_valid, y_valid = valid['features'], valid['labels']
+        X_test, y_test = test['features'], test['labels']
+        X_train, y_train = shuffle(X_train, y_train)
+        
                 
